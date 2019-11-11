a70855d575c71058a673c941f629ab9fc0e6db50
Emmaly <emmaly@emma.ly>
2019-11-11 10:39:58 -0800

    Added Zoho lookup for primary user name/email lookup

--- rename/rename.go
+++ rename/rename.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
+
+	"github.com/gocarina/gocsv"
 
 	"github.com/emmaly/anydesk"
 	"gopkg.in/alecthomas/kingpin.v2"
@@ -14,6 +18,8 @@ import (
 var (
 	licenseID             = kingpin.Flag("license", "AnyDesk License ID").Required().String()
 	apiKey                = kingpin.Flag("apikey", "AnyDesk API Key").Required().String()
+	zohoKey               = kingpin.Flag("zohokey", "Zoho Creator API Key").Required().String() // https://accounts.zoho.com/apiauthtoken/create?SCOPE=ZohoCreator/creatorapi
+	zohoView              = "AnyDesk_Computers"
 	aliasMatch            = regexp.MustCompile(`(?i)^scj(\d+)(-\d+)?@ad$`)
 	newNamespace          = "@scj"
 	longestOldAliasLength = 0
@@ -21,8 +27,33 @@ var (
 	now                   = time.Now()
 )
 
+// ZohoComputer is a computer in Zoho
+type ZohoComputer struct {
+	AssetID                 string `csv:"Asset ID"`
+	WindowsName             string `csv:"Windows Name"`
+	PrimaryUser             string `csv:"Primary User"`
+	PrimaryUserEmailAddress string `csv:"Primary Email Address"`
+}
+
 func main() {
 	kingpin.Parse()
+
+	zohoResp, err := http.Get("https://creator.zoho.com/api/csv/it/view/" + zohoView + "?authtoken=" + (*zohoKey) + "&scope=creatorapi")
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		os.Exit(1)
+	}
+	defer zohoResp.Body.Close()
+	zohoComputers := make([]ZohoComputer, 0)
+	err = gocsv.Unmarshal(zohoResp.Body, &zohoComputers)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		os.Exit(1)
+	}
+	computers := make(map[string]ZohoComputer)
+	for _, zohoComputer := range zohoComputers {
+		computers[zohoComputer.AssetID] = zohoComputer
+	}
 
 	ad, err := anydesk.New(*apiKey, *licenseID, nil)
 	if err != nil {
@@ -70,6 +101,14 @@ func main() {
 		// 	fmt.Printf("Error setting Client ID %d to %s: %s\n", client.ID, newAlias, err.Error())
 		// 	continue
 		// }
-		fmt.Printf("%d = %"+strconv.Itoa(longestOldAliasLength)+"s -> %"+strconv.Itoa(longestNewAliasLength)+"s\n", client.ID, client.Alias, newAlias)
+		var primaryUser, primaryUserName, primaryUserEmail string
+		if computer, ok := computers[assetID]; ok {
+			primaryUserName = computer.PrimaryUser
+			primaryUserEmail = computer.PrimaryUserEmailAddress
+			if primaryUserEmail != "" {
+				primaryUser = strings.TrimSpace(fmt.Sprintf("%s <%s>", primaryUserName, primaryUserEmail))
+			}
+		}
+		fmt.Printf("%d = %"+strconv.Itoa(longestOldAliasLength)+"s -> %"+strconv.Itoa(longestNewAliasLength)+"s\t%s\n", client.ID, client.Alias, newAlias, primaryUser)
 	}
 }
